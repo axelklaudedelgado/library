6dc304d90a001b2cdb5c6c9f754d1b3e282e0533
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-09-12 13:12:14 +0800

    Implement add book to library function logic

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+  const titleValue = document.querySelector("#title").value;
+  const authorValue = document.querySelector("#author").value;
+  const pagesValue = document.querySelector("#pages").value;
+  const checkedValue = document.querySelector('input[name="status"]:checked').value;
+  myLibrary.push(new Book(titleValue, authorValue, pagesValue, checkedValue));
+}
+
+const myLibrary = [
+  new Book("John", "Doe", 50, "Not Read")
+];
+
 const showBtn = document.querySelector("#addButton");
 const dialog = document.querySelector("#modal");
 const modalCloseBtn = dialog.querySelector("#closeButton");
+const submitBook = document.querySelector("#submitBook");
 
 showBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -10,3 +30,9 @@ modalCloseBtn.addEventListener("click", (e) => {
   e.preventDefault();
   dialog.close();
 });
+
+submitBook.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  dialog.close();
+})
