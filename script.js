c556ba7559873d1c9179b36ea088d965100ce1af
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-09-12 14:31:31 +0800

    Add function to display book cards

--- script.js
+++ script.js
@@ -5,6 +5,52 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
+function resetBookCards() {
+  bookCards.innerHTML = "";
+}
+
+function showBookCards() {
+  resetBookCards();
+  for(let book of myLibrary) {
+    createBookCard(book);
+  }
+}
+
+function createBookCard(book) {
+  const card = document.createElement('div');
+  const title = document.createElement('div');
+  const author = document.createElement('div');
+  const pages = document.createElement('div');
+  const readStatus = document.createElement('button');
+  const deleteButton = document.createElement('button');
+
+  card.classList.add("card");
+  readStatus.classList.add("cardButton");
+  deleteButton.classList.add("cardButton");
+  deleteButton.classList.add("redButton");
+  
+  title.textContent = `Title: ${book.title}`;
+  author.textContent = `Author: ${book.author}`;
+  pages.textContent = `Pages: ${book.pages}`;
+
+  if (book.readStatus == "Read") {
+    readStatus.classList.add("greenButton");
+    readStatus.textContent = "Read";
+  } else {
+    readStatus.classList.add("redButton");
+    readStatus.textContent = "Unread";
+  }
+
+  deleteButton.textContent = "Remove";
+
+  bookCards.appendChild(card);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(readStatus);
+  card.appendChild(deleteButton);
+}
+
 function getInputValues() {
   const titleValue = document.querySelector("#title").value;
   const authorValue = document.querySelector("#author").value;
@@ -18,14 +64,13 @@ function addBookToLibrary() {
   myLibrary.push(newBook);
 }
 
-const myLibrary = [
-  new Book("John", "Doe", 50, "Not Read")
-];
+const myLibrary = [];
 
 const showBtn = document.querySelector("#addButton");
 const dialog = document.querySelector("#modal");
 const modalCloseBtn = dialog.querySelector("#closeButton");
 const submitBook = document.querySelector("#submitBook");
+const bookCards = document.querySelector("#bookCards");
 
 showBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -39,5 +84,6 @@ modalCloseBtn.addEventListener("click", (e) => {
 submitBook.addEventListener("click", (event) => {
   event.preventDefault();
   addBookToLibrary();
+  showBookCards();
   dialog.close();
 })
